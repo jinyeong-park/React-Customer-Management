3f8a2b1ec7d005cab871c285bc836cdb48d6f0cf
jinyeong-park <jinyeongpark1205@gmail.com>
2020-03-12 21:58:13 -0700

    Bring Customer components in App class (App.js)

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import React, {Component } from 'react';
-import logo from './logo.svg';
+import Customer from './components/Customer'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="gray-background">
-        <img src={logo} lat="logo"/>
-        <h2>Let's develop management system!</h2>
+      <Customer/>
 
-      </div>
     );
   }
 }
---
+++ src/components/Customer.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Customer extends React.Component {
+    render() {
+        return (
+            <div>
+                <h2>Jinyeong Park</h2>
+                <p>96122</p>
+                <p>female</p>
+                <p>University student</p>
+            
+             </div>
+
+        )
+
+    }
+}
+export default Customer;
