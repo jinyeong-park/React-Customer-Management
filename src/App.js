91d8ba5f5dd765c0659caa46e9a6e8d3cecc59ae
jinyeong-park <jinyeongpark1205@gmail.com>
2020-03-13 00:45:11 -0700

    apply Material- Ui (Horizontal scroll bar) in App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React, {Component } from 'react';
 import Customer from './components/Customer'
 import './App.css';
+import Paper from '@material-ui/core/Paper'
 import Table from '@material-ui/core/Table';
 import TableHead from '@material-ui/core/TableHead';
 import TableBody from '@material-ui/core/TableBody';
 import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
+import { withStyles} from '@material-ui/core/styles';
+
+const styles = theme => ({
+  root: {
+    width: '100%',
+    marginTop: theme.spacing.unit * 3,
+    autoflowX: "auto"
+  },
+  table: {
+    minWidth: 1080
+  }
+})
 
 const customers = [
 {
@@ -36,9 +49,11 @@ const customers = [
 
 class App extends Component {
   render() {
+    const { classes } = this.props;
     return (
-      <div>
-        <Table>
+      <Paper className={classes.root}>
+      {/* <div> */}
+        <Table className={classes.table}>
           <TableHead>
             <TableRow>
               <TableCell>No.</TableCell>
@@ -58,7 +73,8 @@ class App extends Component {
           </TableBody>
         </Table>
         
-      </div>
+      {/* </div> */}
+      </Paper>
     )
   }
 }
@@ -89,4 +105,4 @@ class App extends Component {
       //</div>
   
 
-export default App;
+export default withStyles(styles)(App);
