22e57ae41c309d68c5d15f0483114840776d67b8
jinyeong-park <jinyeongpark1205@gmail.com>
2020-03-15 20:21:47 -0700

    Creaete App bar with search & Apply web font

--- client/src/App.js
+++ client/src/App.js
@@ -10,20 +10,94 @@ import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
 import { withStyles } from '@material-ui/core/styles';
 import CircularProgress from '@material-ui/core/CircularProgress'
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import InputBase from '@material-ui/core/InputBase';
+import { fade } from '@material-ui/core/styles';
+import MenuIcon from '@material-ui/icons/Menu';
+import SearchIcon from '@material-ui/icons/Search';
+
 
 const styles = theme => ({
   root: {
     width: '100%',
-    marginTop: theme.spacing(3),
-    overflowX: "auto"
-  },
-  table: {
     minWidth: 1080
+    //marginTop: theme.spacing(3),
+    //overflowX: "auto"
+  },
+  menu: {
+    marginTop: 15,
+    marginBottom: 15,
+    display: 'flex',
+    justifyContent: 'center'
+  },
+  paper: {
+    marginLeft: 18,
+    marginRight: 18
   },
   progress: {
     margin: theme.spacing(2)
+  },
+
+  grow: {
+    flexGrow: 1,
+  },
+  tableHead: {
+    fontSize: '1.0rem'
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+    display: 'none',
+    [theme.breakpoints.up('sm')]: {
+      display: 'block',
+    },
+  },
+  search: {
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: fade(theme.palette.common.white, 0.25),
+    },
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing(1),
+      width: 'auto',
+    },
+  },
+  searchIcon: {
+    width: theme.spacing(7),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  inputRoot: {
+    color: 'inherit',
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 7),
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      width: 120,
+      '&:focus': {
+        width: 200,
+      },
+    },
   }
 });
+  
+
+
 // React component life cycle: 1) constructor() -> 2) componentWillMount() -> 3) render() -> 4) componentDidMount()
 // props or state => shouldComponentUpdate() -> 3) render (draw component again) 
 
@@ -74,21 +148,49 @@ class App extends Component {
   }
 
 
-  render() {
-    const { classes } = this.props;
-    return (
-    <div>
-      <Paper className={classes.root}>
+render() {
+  const { classes } = this.props;
+  const cellList = ["No", "Profile Image", "Name", "Birthday", "Gender", "Job", "Delete"]
+  return (
+  <div className={classes.root}>
+    <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="open drawer"
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography className={classes.title} variant="h6" noWrap>
+            Customer Management System
+          </Typography>
+          <div className={classes.search}>
+            <div className={classes.searchIcon}>
+              <SearchIcon />
+            </div>
+            <InputBase
+              placeholder="Search"
+              classes={{
+                root: classes.inputRoot,
+                input: classes.inputInput,
+              }}
+              inputProps={{ 'aria-label': 'search' }}
+            />
+          </div>
+        </Toolbar>
+      </AppBar>
+      <div className={classes.menu}>
+        <CustomerAdd stateRefresh={this.stateRefresh}/>
+      </div>
+      <Paper className={classes.paper}>
         <Table className={classes.table}>
           <TableHead>
             <TableRow>
-              <TableCell>No.</TableCell>
-              <TableCell>Image</TableCell>
-              <TableCell>Name</TableCell>
-              <TableCell>Birthday</TableCell>
-              <TableCell>Gender</TableCell>
-              <TableCell>Job</TableCell>
-              <TableCell>Setting</TableCell>
+              {cellList.map(c => {
+                return <TableCell className={classes.tableHead}>{c}</TableCell>
+              })}
             </TableRow>
           </TableHead>
           <TableBody>
@@ -107,32 +209,6 @@ class App extends Component {
     </div>
     );
   }
-}
-      // {/* <Customer
-      // id={customers[0].id}
-      // image={customers[0].image}
-      // name={customers[0].name}
-      // birthday={customers[0].birthday}
-      // gender={customers[0].gender}
-      // job={customers[0].job}
-      // />
-      // <Customer
-      // id={customers[1].id}
-      // image={customers[1].image}
-      // name={customers[1].name}
-      // birthday={customers[1].birthday}
-      // gender={customers[1].gender}
-      // job={customers[1].job}
-      // />
-      // <Customer
-      // id={customers[2].id}
-      // image={customers[2].image}
-      // name={customers[2].name}
-      // birthday={customers[2].birthday}
-      // gender={customers[2].gender}
-      // job={customers[2].job}
-      // /> */}
-      // //</div>
-  
+ };
 
 export default withStyles(styles)(App);
